9fbf0c948a650e8a0b6ec6d2b2bb41254c382377
Tawan.DEV <88831519+Tawan8645@users.noreply.github.com>
2025-10-20 23:15:42 +0700

    Delete Robomec 2_hardware esp32  directory

--- Robomec 2_hardware esp32 /firmware_imu_custom/lib/battery/battery.h
+++
@@ -1,8 +0,0 @@
-#ifndef BATTERY_H
-#define BATTERY_H
-
-#include <sensor_msgs/msg/battery_state.h>
-sensor_msgs__msg__BatteryState getBattery();
-void initBattery();
-
-#endif
--- Robomec 2_hardware esp32 /firmware_imu_custom/lib/range/range.h
+++
@@ -1,8 +0,0 @@
-#ifndef RANGE_H
-#define RANGE_H
-
-#include <sensor_msgs/msg/range.h>
-sensor_msgs__msg__Range getRange();
-void initRange();
-
-#endif
